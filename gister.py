d865653ba79b22f0d940cdf37e45fe3670438bc3
Gregory Liras <gregliras@gmail.com>
2012-02-25 17:00:19 +0200

    cleared pychecker

--- gister.py
+++ gister.py
@@ -7,7 +7,7 @@
 #
 #* Creation Date : 16-02-2012
 #
-#* Last Modified : Sat 25 Feb 2012 04:58:54 PM EET
+#* Last Modified : Sat 25 Feb 2012 05:00:01 PM EET
 #
 #* Created By : Greg Liras <gregliras@gmail.com>
 #
@@ -45,7 +45,7 @@ def main():
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
     br.set_handle_robots(False)
-    r = br.open(URL)
+    br.open(URL)
 
     br.select_form( nr = 1 ) 
     br["login"] = username
